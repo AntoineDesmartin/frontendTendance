6159e9ec48ad9f77b91f33de8b6ce887dc0f70bf
Ines <ines.oliete@gmail.com>
2023-07-31 12:24:41 +0200

    Mapscreen : ajout current position

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 import {
   Image,
   ImageBackground,
@@ -9,14 +9,39 @@ import {
   TextInput,
   TouchableOpacity,
   View,
-} from 'react-native';
-
+} from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import * as Location from "expo-location";
 
 export default function MapScreen() {
+  const [currentPosition, setCurrentPosition] = useState(null);
 
-    return (
-        <View>
-            <Text>MapScreen</Text>
-        </View>
-    );
+  useEffect(() => {
+    (async () => {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+
+      if (status === "granted") {
+        Location.watchPositionAsync({ distanceInterval: 10 }, (location) => {
+          setCurrentPosition(location.coords);
+        });
+      }
+    })();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <MapView style={styles.map}>
+        {currentPosition && <Marker coordinate={currentPosition} title="My position" pinColor="#fecb2d" />}
+      </MapView>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    flex: 1,
+  },
+});
