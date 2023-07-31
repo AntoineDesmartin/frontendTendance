f352c9bcdb9886a85370d2e6d5d07f5f11305cbc
Desmartin Antoine <desmartin.aantoine@gmail.com>
2023-07-31 12:21:13 +0200

    commit test

---
+++ screens/EventScreen.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import {
+  Image,
+  ImageBackground,
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+
+export default function EventScreen() {
+
+    return (
+        <View>
+            <Text>EventScreen</Text>
+        </View>
+    );
+}
