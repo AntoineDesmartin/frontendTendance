b44771e6d3d8c3cfb67c226f8f6227d3d97d48d2
Desmartin Antoine <desmartin.aantoine@gmail.com>
2023-08-02 15:19:43 +0200

    push pour dynamiser le site

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -31,171 +31,7 @@ import { setEvent } from '../reducers/event';
 
 
 export default function ProfileScreen(props) {
-    const eventData = [{
-        creator: 'joan',
-        eventName: 'SJparty',
-        type: 'music',
-        date: '2023-08-03T09:32:34.633Z',
-        hourStart: '22:45',
-        hourEnd: '01:45',
-        address: '35 rue de Marseille 69007 Lyon',
-        price: 'Gratuit',
-        website: 'linktr.ee/sacrejojo69',
-        description: 'Lorem ipsum dolor sit amet. Qui voluptates internos nam inventore atque aut culpa repellendus ut velit officia. Et velit vero sed velit reiciendis ut accusantium dolorem cum voluptates corporis sit quidem architecto.',
-        eventCover: '',
-        users: {
-            interUsers: ['001', '002', '003', '004'],
-            partUsers: ['005', '006', '007', '008'],
-        },
-    }, {
-        creator: 'nico',
-        eventName: 'TacosMania',
-        type: 'food',
-        date: '2023-08-03T09:32:34.633Z',
-        hourStart: '19:45',
-        hourEnd: '22:45',
-        address: '26 Rue de Marseille 69007 Lyon',
-        price: '10',
-        website: 'https://www.kebab-frites.com/kebab/uskudar-lyon-7.html',
-        description: 'Lorem ipsum dolor sit amet. Qui voluptates internos nam inventore atque aut culpa repellendus ut velit officia. Et velit vero sed velit reiciendis ut accusantium dolorem cum voluptates corporis sit quidem architecto.',
-        eventCover: '',
-        users: {
-            interUsers: ['001', '002', '003', '004'],
-            partUsers: ['005', '006', '007', '008','09','10'],
-        },
-    }, 
-    {
-        creator: 'mich',
-        eventName: 'ExpoMeme23',
-        type: 'art',
-        date: '2023-07-31T09:32:34.633Z',
-        hourStart: '22:45',
-        hourEnd: '05:45',
-        address: '20 Place des Terreaux 69001 Lyon',
-        price: '5',
-        website: 'https://www.mba-lyon.fr/fr',
-        description: 'Lorem ipsum dolor sit amet. Qui voluptates internos nam inventore atque aut culpa repellendus ut velit officia. Et velit vero sed velit reiciendis ut accusantium dolorem cum voluptates corporis sit quidem architecto.',
-        eventCover: '',
-        users: {
-            interUsers: ['001', '002'],
-            partUsers: ['003', '004','005', '009'],
-        },
-    }, {
-        creator: 'adri',
-        eventName: 'Marathon de Lyon',
-        type: 'sport',
-        date: '2023-07-31T09:32:34.633Z',
-        hourStart: '05:45',
-        hourEnd: '22:45',
-        address: 'Place Bellecour 69002 Lyon',
-        price: '25',
-        website: 'https://www.runinlyon.com/fr',
-        description: 'Lorem ipsum dolor sit amet. Qui voluptates internos nam inventore atque aut culpa repellendus ut velit officia. Et velit vero sed velit reiciendis ut accusantium dolorem cum voluptates corporis sit quidem architecto.',
-        eventCover: '',
-        users: {
-            interUsers: ['001', '002'],
-            partUsers: ['003', '004','005', '006', '008'],
-        },
-    }, {
-        creator: 'ines',
-        eventName: 'Les Matins de la Cartographie',
-        type: 'science',
-        date: '2023-07-31T09:32:34.633Z',
-        hourStart: '05:45',
-        hourEnd: '11:45',
-        address: 'Place de la Nation, 69120 Vaulx-en-Velin',
-        price: '10',
-        website: 'https://www.planetariumvv.com/',
-        description: 'Lorem ipsum dolor sit amet. Qui voluptates internos nam inventore atque aut culpa repellendus ut velit officia. Et velit vero sed velit reiciendis ut accusantium dolorem cum voluptates corporis sit quidem architecto.',
-        eventCover: '',
-        users: {
-            interUsers: ['001', '002'],
-            partUsers: ['003', '004','005', '006'],
-        },
-    }, {
-        creator: 'tone',
-        eventName: 'La Pêche au Harpon',
-        type: 'nature',
-        date: '2023-07-31T09:32:34.633Z',
-        hourStart: '05:45',
-        hourEnd: '00:45',
-        address: 'Rue de Créqui 69006 Lyon',
-        price: '55',
-        website: 'https://www.snsm.org/conseils/conseils-loisirs-nautiques/peche-sous-marine-pratique-et-reglementation',
-        description: 'Lorem ipsum dolor sit amet. Qui voluptates internos nam inventore atque aut culpa repellendus ut velit officia. Et velit vero sed velit reiciendis ut accusantium dolorem cum voluptates corporis sit quidem architecto.',
-        eventCover: '',
-        users: {
-            interUsers: ['001', '002'],
-            partUsers: ['003', '004','005', '006'],
-        },
-    }, {
-        creator: 'max',
-        eventName: 'DemodayBatch89-LaCapsuleGrooveCamp',
-        type: 'science',
-        date: '2023-07-31T09:32:34.633Z',
-        hourStart: '14:45',
-        hourEnd: '16:45',
-        address: '35 rue de Marseille 69007 Lyon',
-        price: '10',
-        website: 'https://now-coworking.com/',
-        description: 'Lorem ipsum dolor sit amet. Qui voluptates internos nam inventore atque aut culpa repellendus ut velit officia. Et velit vero sed velit reiciendis ut accusantium dolorem cum voluptates corporis sit quidem architecto.',
-        eventCover: '',
-        users: {
-            interUsers: ['001', '002', '009', '010'],
-            partUsers: ['003', '004','005', '006', '007', '008'],
-        },
-    }, 
-    {
-        creator: 'user',
-        eventName: '???',
-        type: 'science',
-        date: '2023-07-31T09:32:34.633Z',
-        hourStart: '00:45',
-        hourEnd: '03:45',
-        address: '35 rue de Marseille 69007 Lyon',
-        price: 'Gratuit',
-        website: '',
-        description: 'Lorem ipsum dolor sit amet. Qui voluptates internos nam inventore atque aut culpa repellendus ut velit officia. Et velit vero sed velit reiciendis ut accusantium dolorem cum voluptates corporis sit quidem architecto.',
-        eventCover: '',
-        users: {
-            interUsers: ['003', '004','005', '006', '007', '008', '001', '002', '009', '010'],
-            partUsers: [],
-        },
-    }, 
-    {
-        creator: 'user',
-        eventName: 'TekTek69event',
-        type: 'music',
-        date: '2023-07-31T09:32:34.633Z',
-        hourStart: '21:45',
-        hourEnd: '03:45',
-        address: 'rue de Sèze 69006 Lyon',
-        price: '3',
-        website: '',
-        description: 'Lorem ipsum dolor sit amet. Qui voluptates internos nam inventore atque aut culpa repellendus ut velit officia. Et velit vero sed velit reiciendis ut accusantium dolorem cum voluptates corporis sit quidem architecto.',
-        eventCover: '',
-        users: {
-            interUsers: ['003', '004','005', '006', '007', '008', '001', '002', '009', '010'],
-            partUsers: [],
-        },
-    }, 
-    {
-        creator: 'user',
-        eventName: 'GymTonic2000',
-        type: 'sport',
-        date: '2023-07-31T09:32:34.633Z',
-        hourStart: '07:45',
-        hourEnd: '08:45',
-        address: 'rue de Sèze 69006 Lyon',
-        price: '3',
-        website: '',
-        description: 'Lorem ipsum dolor sit amet. Qui voluptates internos nam inventore atque aut culpa repellendus ut velit officia. Et velit vero sed velit reiciendis ut accusantium dolorem cum voluptates corporis sit quidem architecto.',
-        eventCover: '',
-        users: {
-            interUsers: ['003', '004','005', '006', '002', '009', '010'],
-            partUsers: ['007', '008', '001'],
-        },
-    }];
+
     
     const dispatch = useDispatch();
     // todo Gerer AMIS/MESSAGERIE/FAVORIS/PARAMETRE
@@ -217,9 +53,9 @@ useEffect(() => {
     })
     .then(response => response.json())
     .then(data => {
-        //! on doit gerer ici les events selon leur date et creer comme en dessous past and futur EVENT
-
+        // console.log("data fetch profile event",data);
       const eventsFutur = data.map((data, index) => (
+        
         <Pressable onPress={() => handlePress(data)} key={`futur-${index}`}>
           <Event data={data} />
         </Pressable>
@@ -227,12 +63,7 @@ useEffect(() => {
       ));
       setFuturEvents(eventsFutur);
 
-    //   const eventsPast = data.map((data, index) => (
-    //     <Pressable onPress={() => handlePress(data)} key={`past-${index}`}>
-    //       <Event data={data} />
-    //     </Pressable>
-    //   ));
-    //   setPastEvents(eventsPast);
+    
       
     });
   } else {
@@ -241,7 +72,12 @@ useEffect(() => {
   }
 }, [user]);
 
-
+//   const eventsPast = data.map((data, index) => (
+    //     <Pressable onPress={() => handlePress(data)} key={`past-${index}`}>
+    //       <Event data={data} />
+    //     </Pressable>
+    //   ));
+    //   setPastEvents(eventsPast);
         
 
 //! Function _____________________________________________________________________________________________________________________________
@@ -259,14 +95,14 @@ useEffect(() => {
     // })
 
     
-    const pastEvents = eventData.map((data,index)=>{
-        let date = new Date(data.date);
-        let now = new Date()
+    // const pastEvents = eventData.map((data,index)=>{
+    //     let date = new Date(data.date);
+    //     let now = new Date()
         
-        if(date<now){
-            return <Pressable key={`past-${index}`} onPress={()=>handlePress(data)}><Event data={data} ></Event></Pressable>
-        }
-    })
+    //     if(date<now){
+    //         return <Pressable key={`past-${index}`} onPress={()=>handlePress(data)}><Event data={data} ></Event></Pressable>
+    //     }
+    // })
 
 
 
@@ -310,7 +146,7 @@ useEffect(() => {
             </View>
             
             <View style={styles.viewName}>
-                <Text>Mr Tone</Text>
+                <Text>{user.username}</Text>
             </View>
             
 
@@ -341,13 +177,13 @@ useEffect(() => {
             <ScrollView style={styles.events}>
 
                 <View style={styles.futurEvents}>
-                    <Text style={styles.text}>Evenement a venir _____________________________</Text>
+                    <Text style={styles.text}> ____________________ Events ______________________</Text>
                     {futurEvents}
                 </View>
 
                 <View style={styles.pastEvents}>
-                <Text style={styles.text}>Evenement Passée _____________________________</Text>
-                    {pastEvents}
+                {/* <Text style={styles.text}>Evenement Passée _____________________________</Text> */}
+                    {/* {pastEvents} */}
                 </View>
 
                 
